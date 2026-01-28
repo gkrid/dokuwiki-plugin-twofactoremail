6099d5b12ed90ee091a638f42829a5acb9a66306
Szymon Olewniczak <szyolewn@pg.edu.pl>
2026-01-28 12:42:57 +0100

    polish translation

---
+++ lang/pl/lang.php
@@ -0,0 +1,8 @@
+<?php
+$lang['name'] = 'E-Mail';
+
+$lang['verifynotice'] = "Kod weryfikacyjny został wysłany na Twój adres e-mail.  Jeśli nie otrzymasz go w ciągu kilku minut, skontaktuj się z administratorem wiki.";
+
+$lang['codesent'] = 'Wysłano jednorazowy kod. Sprawdź swoją skrzynkę e-mail.';
+$lang['codesentfail'] = 'Nie udało się wysłać kodu. Skontaktuj się z administratorem w celu uzyskania pomocy.';
+$lang['subject'] = 'Twój kod logowania';
---
+++ lang/pl/settings.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * polish language file for twofactoraltemail plugin
+ */
+
+$lang['tolerance'] = 'Liczba 30-sekundowych przedziałów czasowych po których przesłany kod traci ważność.';
+
